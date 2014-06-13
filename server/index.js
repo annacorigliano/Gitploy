547d620408d7fce4394c55342b37c1920c77789c
Stephen Parente <stephen@ingenuitydesign.com>
2014-06-13 12:19:27 -0400

    Added tunneling to request body.

--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@
 
 var express         = require('express'),
     bodyparser      = require('body-parser'),
-    when            = require('when');
+    when            = require('when'),
+    tunnel          = require('./tunnel');
 
 module.exports.start = function() {
 
@@ -14,8 +15,16 @@ module.exports.start = function() {
         app.post('/:app/recieve', function(req, res){
             var appName = req.params.app;
             var data = req.body;
+            res.end('Thanks git.'); //we got the data we need from git don't hang it up
+            //we would normally get configuration data for posted app and check it against the secret
+            // @todo
 
-            res.end('Thanks git.');
+            if (true) {
+                //now we do this
+                var Tunnel = tunnel('localhost');
+                Tunnel.ping(data);
+            }
+
         });
 
         try {
